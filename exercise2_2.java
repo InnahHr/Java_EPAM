b3cbb4c0638bd96810b3cc7e233cd0a4e9e8e8f9
InnahHr <46709648+InnahHr@users.noreply.github.com>
2019-07-11 19:10:22 +0300

    Task 2 Exercise 2: Ветвления

--- exercise2_2.java
+++ exercise2_2.java
@@ -3,7 +3,6 @@ package com.company;
 import java.util.Scanner;
 
 public class exercise2_2 {
-
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.println ("Please enter  A ");
